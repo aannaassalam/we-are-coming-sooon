2dcc91c3b3a91a8081e3b0373d81b791008219cc
Anas Alam <anasalam027@gmail.com>
2020-09-19 15:23:17 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, BrowserRouter } from "react-router-dom";
 import ComingSoon from "./pages/comingSoon/comingSoon";
